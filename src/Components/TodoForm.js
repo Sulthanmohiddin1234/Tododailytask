fdc50b70c842064ef2b392479ab9c91566524c45
Sulthan <sulthanmohiddin1234@gmail.com>
2021-07-02 11:34:10 +0700

    todolist app

---
+++ src/Components/TodoForm.js
@@ -0,0 +1,58 @@
+import React, {useState} from "react";
+import {
+    FormGroup,
+    Input,
+    InputGroup,
+    InputGroupAddon,
+    Button,
+    Form,
+    Container,
+} from "reactstrap";
+import {v4} from "uuid";
+
+const TodoForm = ({addTodos}) => {
+    const [todoString, SetTodoString] = useState("")
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        if (todoString === "") {
+            return alert("please fill some value")
+        }
+
+        const todo = {
+             todoString,
+            id: v4()
+        }
+        //TODO:
+        addTodos(todo)
+            SetTodoString("")
+    }
+
+
+    return(
+        <Form onSubmit={handleSubmit}>
+        <FormGroup>
+        
+        <InputGroup>
+        <Input
+        type="text"
+        name="todo"
+        id="todo"
+        placeholder="Enter a todo string"
+        value={todoString}
+        onChange={e => SetTodoString(e.target.value)}
+        />
+        <InputGroupAddon addonType="prepend">
+        <Button
+        color="success"
+
+        >Add Todo</Button>
+        </InputGroupAddon>
+        </InputGroup>
+        
+        </FormGroup>
+        </Form>
+    )
+} 
+
+export default TodoForm;
